5920b094424d855a2972bbb09e23c2955fd90412
Daniel Lindo <dilindo28@gmail.com>
2019-07-21 16:15:24 -0700

    fix store compatability outside of Chrome

--- client/src/store.js
+++ client/src/store.js
@@ -19,13 +19,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-export const store = createStore(persistedReducer, initialState, compose(
-	applyMiddleware(...middleware),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+export const store = createStore(
+	persistedReducer,
+	initialState,
+	composeEnhancers(applyMiddleware(...middleware))
+);
 
 export const persistor = persistStore(store);
-
-
