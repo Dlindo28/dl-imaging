fc242c7d1cf20e34208ee257ad648dddbc00639a
Dlindo28 <dilindo28@gmail.com>
2020-07-20 13:12:54 -0700

    added email link

--- client/lib/components/ContactLinks.js
+++ client/lib/components/ContactLinks.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports.default = void 0;
 
@@ -15,43 +15,108 @@ var _freeBrandsSvgIcons = require("@fortawesome/free-brands-svg-icons");
 
 require("../css/ContactLinks.css");
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  } else {
+    var newObj = {};
+    if (obj != null) {
+      for (var key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+          var desc =
+            Object.defineProperty && Object.getOwnPropertyDescriptor
+              ? Object.getOwnPropertyDescriptor(obj, key)
+              : {};
+          if (desc.get || desc.set) {
+            Object.defineProperty(newObj, key, desc);
+          } else {
+            newObj[key] = obj[key];
+          }
+        }
+      }
+    }
+    newObj.default = obj;
+    return newObj;
+  }
+}
 
 class ContactLinks extends _react.Component {
   render() {
-    return dom("div", {
-      className: "ContactLinks"
-    }, dom("ul", null, dom("li", null, dom("a", {
-      href: "mailto:dilindo28@gmail.com",
-      className: "contactItem",
-      target: "_blank",
-      rel: "noopener noreferrer"
-    }, dom(_reactFontawesome.FontAwesomeIcon, {
-      icon: _freeSolidSvgIcons.faEnvelope
-    }))), dom("li", null, dom("a", {
-      href: "https://www.instagram.com/dl.aesthetics/",
-      className: "contactItem",
-      target: "_blank",
-      rel: "noopener noreferrer"
-    }, dom(_reactFontawesome.FontAwesomeIcon, {
-      icon: _freeBrandsSvgIcons.faInstagram
-    }))), dom("li", null, dom("a", {
-      href: "https://github.com/Dlindo28",
-      className: "contactItem",
-      target: "_blank",
-      rel: "noopener noreferrer"
-    }, dom(_reactFontawesome.FontAwesomeIcon, {
-      icon: _freeBrandsSvgIcons.faGithub
-    }))), dom("li", null, dom("a", {
-      href: "https://www.linkedin.com/in/daniel-lindo-772894142/",
-      className: "contactItem",
-      target: "_blank",
-      rel: "noopener noreferrer"
-    }, dom(_reactFontawesome.FontAwesomeIcon, {
-      icon: _freeBrandsSvgIcons.faLinkedin
-    })))));
+    return dom(
+      "div",
+      {
+        className: "ContactLinks",
+      },
+      dom(
+        "ul",
+        null,
+        dom(
+          "li",
+          null,
+          dom(
+            "a",
+            {
+              href: "mailto:dlimaging.photo@gmail.com",
+              className: "contactItem",
+              target: "_blank",
+              rel: "noopener noreferrer",
+            },
+            dom(_reactFontawesome.FontAwesomeIcon, {
+              icon: _freeSolidSvgIcons.faEnvelope,
+            })
+          )
+        ),
+        dom(
+          "li",
+          null,
+          dom(
+            "a",
+            {
+              href: "https://www.instagram.com/dl.aesthetics/",
+              className: "contactItem",
+              target: "_blank",
+              rel: "noopener noreferrer",
+            },
+            dom(_reactFontawesome.FontAwesomeIcon, {
+              icon: _freeBrandsSvgIcons.faInstagram,
+            })
+          )
+        ),
+        dom(
+          "li",
+          null,
+          dom(
+            "a",
+            {
+              href: "https://github.com/Dlindo28",
+              className: "contactItem",
+              target: "_blank",
+              rel: "noopener noreferrer",
+            },
+            dom(_reactFontawesome.FontAwesomeIcon, {
+              icon: _freeBrandsSvgIcons.faGithub,
+            })
+          )
+        ),
+        dom(
+          "li",
+          null,
+          dom(
+            "a",
+            {
+              href: "https://www.linkedin.com/in/daniel-lindo-772894142/",
+              className: "contactItem",
+              target: "_blank",
+              rel: "noopener noreferrer",
+            },
+            dom(_reactFontawesome.FontAwesomeIcon, {
+              icon: _freeBrandsSvgIcons.faLinkedin,
+            })
+          )
+        )
+      )
+    );
   }
-
 }
 
 var _default = ContactLinks;
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,7 @@ import {
   faLinkedin,
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
@@ -17,6 +18,11 @@ const Footer = () => {
               <div className="nav-wrapper container">
                 <span>© 2020 Copyright</span>
                 <ul className="right">
+                  <li>
+                    <a href="mailto:dlimaging.photo@gmail.com">
+                      <FontAwesomeIcon icon={faEnvelope} />
+                    </a>
+                  </li>
                   <li>
                     <a href="https://www.instagram.com/dl.imaging/">
                       <FontAwesomeIcon icon={faInstagram} />
